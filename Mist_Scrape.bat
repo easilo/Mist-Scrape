1f43f94478a01e5393f14cc2f2560437ffa5445b
Erin Asilo <erinasilo21@gmail.com>
2022-10-16 15:34:15 -0700

    Updated .bat file to new path

--- Mist_Scrape.bat
+++ Mist_Scrape.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 
-python "%USERPROFILE%\Documents\Erin_Automation\Mist_Scrape\run_files\main.py"
+python "%USERPROFILE%\Documents\Erin_Automation\Mist-Scrape\run_files\main.py"
 exit /b 0
